dfb31135b4bdeb4015232679a5873a03698cf4f3
RomainDW <romain.ollier34@gmail.com>
2019-01-18 15:02:05 +0100

    fix php syntax

--- src/DataFixtures/TrickFixture.php
+++ src/DataFixtures/TrickFixture.php
@@ -10,15 +10,15 @@ class TrickFixture extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-		for ($i = 0; $i < 20; $i++) {
-			$trick = new Trick();
-			$trick->setTitle('Trick n°'.$i);
-			$trick->setSlug('trick-'.$i);
-			$trick->setDescription('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+        for ($i = 0; $i < 20; ++$i) {
+            $trick = new Trick();
+            $trick->setTitle('Trick n°'.$i);
+            $trick->setSlug('trick-'.$i);
+            $trick->setDescription('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
 			tempor incididunt ut labore et dolore magna aliqua. Sit amet cursus sit amet dictum sit amet justo.');
-			$trick->setCreatedAt(new \DateTime);
-			$manager->persist($trick);
-		}
+            $trick->setCreatedAt(new \DateTime());
+            $manager->persist($trick);
+        }
 
         $manager->flush();
     }
