265cba1d8d4503f7b38bd8afb4104257a575e102
RomainDW <romain.ollier34@gmail.com>
2019-01-19 16:29:55 +0100

    creation of a method to obtain the figures associated with the requested page in ajax

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -14,17 +14,65 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+    // define how many results you want per page
+    private $results_per_page = 6;
+
     /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \Doctrine\ORM\NonUniqueResultException
      * @Route("/", name="homepage")
      */
     public function index()
     {
-        $tricks = $this->getDoctrine()
-            ->getRepository(Trick::class)
-            ->findAll();
+        $manager = $this->getDoctrine()->getRepository(Trick::class);
+
+        // find out the number of results stored in database
+        $number_of_tricks = $manager->getNumberOfTricks();
+
+        // determine number of total pages available
+        $number_of_pages = ceil($number_of_tricks / $this->results_per_page);
+
+        // retrieve selected results from database and display them on page
+        $tricks = $manager->getTricksPagination(1, $this->results_per_page);
 
         return $this->render('homepage/index.html.twig', [
             'tricks' => $tricks,
+            'current_page' => 1,
+            'total_page' => $number_of_pages,
+        ]);
+    }
+
+    /**
+     * @param $page
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \Doctrine\ORM\NonUniqueResultException
+     * @Route("/ajax-pagination/{page}", name="pagination", methods={"POST"})
+     */
+    public function pagination($page)
+    {
+        $manager = $this->getDoctrine()->getRepository(Trick::class);
+
+        // find out the number of results stored in database
+        $number_of_tricks = $manager->getNumberOfTricks();
+
+        // determine number of total pages available
+        $number_of_pages = ceil($number_of_tricks / $this->results_per_page);
+
+        // Minimum 1 page
+        if (0 == $number_of_pages) {
+            $number_of_pages = 1;
+        }
+
+        // determine the sql LIMIT starting number for the results on the displaying page
+        $this_page_first_result = ($page - 1) * $this->results_per_page;
+
+        // retrieve selected results from database and display them on page
+        $tricks = $manager->getTricksPagination($this_page_first_result, $this->results_per_page);
+
+        return $this->render('homepage/_partials/content.html.twig', [
+            'tricks' => $tricks,
+            'current_page' => $page,
+            'total_page' => $number_of_pages,
         ]);
     }
 }
