a70aa073650f598fd3e5057d10e912a33380e0b7
RomainDW <romain.ollier34@gmail.com>
2019-01-28 22:34:07 +0100

    Create a registerVerification method to register the user after the verification email

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -5,10 +5,12 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\RegistrationFormType;
 use App\Security\LoginFormAuthenticator;
+use App\Service\AccountVerification;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
 
@@ -24,6 +26,8 @@ class RegistrationController extends AbstractController
      * @param \Swift_Mailer                $mailer
      *
      * @return Response
+     *
+     * @throws \Exception
      */
     public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, GuardAuthenticatorHandler $guardHandler, LoginFormAuthenticator $authenticator, \Swift_Mailer $mailer): Response
     {
@@ -32,13 +36,11 @@ class RegistrationController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $vkey = md5(random_bytes(10));
             // encode the plain password
-            $user->setPassword(
-                $passwordEncoder->encodePassword(
-                    $user,
-                    $form->get('plainPassword')->getData()
-                )
-            )->setRoles(['ROLE_USER_NOT_VERIFIED']);
+            $user->setPassword($passwordEncoder->encodePassword($user, $form->get('plainPassword')->getData()))
+                ->setVkey($vkey)
+                ->setRoles(['ROLE_USER_NOT_VERIFIED']);
 
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($user);
@@ -50,7 +52,7 @@ class RegistrationController extends AbstractController
                 ->setBody(
                     $this->renderView(
                         'emails/registration.html.twig',
-                        ['name' => $form->get('email')]
+                        ['name' => $form->get('email'), 'vkey' => $vkey]
                     ),
                     'text/html'
                 )
@@ -68,16 +70,35 @@ class RegistrationController extends AbstractController
 
             $mailer->send($message);
 
-            return $guardHandler->authenticateUserAndHandleSuccess(
-                $user,
-                $request,
-                $authenticator,
-                'main' // firewall name in security.yaml
-            );
+            return $this->render('registration/registration-pre-validation.html.twig', [
+                'userEmail' => $user->getEmail(),
+            ]);
         }
 
         return $this->render('registration/register.html.twig', [
             'registrationForm' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/verification/{vkey}", name="app_verification", methods={"GET"})
+     *
+     * @param User                $user
+     * @param AccountVerification $accountVerification
+     *
+     * @return Response
+     */
+    public function registerVerification(User $user, AccountVerification $accountVerification)
+    {
+        $verification = $accountVerification->verification($user);
+
+        // Connexion à mettre dans service
+        $token = new UsernamePasswordToken($user, null, 'main', $user->getRoles());
+        $this->get('security.token_storage')->setToken($token);
+        $this->get('session')->set('_security_secured_area', serialize($token));
+
+        return $this->render('registration/registration-confirmed.html.twig', [
+            'verification' => $verification,
         ]);
     }
 }
