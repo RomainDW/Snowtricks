9a5090c392c4f3399533a337a75b33e1f9115d59
RomainDW <romain.ollier34@gmail.com>
2019-01-28 22:36:16 +0100

    adding a verification key to the user fixture

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -16,14 +16,20 @@ class UserFixtures extends Fixture
         $this->passwordEncoder = $passwordEncoder;
     }
 
+    /**
+     * @param ObjectManager $manager
+     * @throws \Exception
+     */
     public function load(ObjectManager $manager)
     {
         $user = new User();
+        $vkey = md5(random_bytes(10));
 
         $user->setPassword($this->passwordEncoder->encodePassword(
             $user, 'password'))
             ->setEmail('user@email.com')
-            ->setRoles(['ROLE_USER']);
+            ->setRoles(['ROLE_USER'])
+            ->setVkey($vkey);
 
         $manager->persist($user);
         $manager->flush();
