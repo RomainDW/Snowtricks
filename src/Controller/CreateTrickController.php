385f5bb2bc1f6bac641e82564e2a8911eed54816
RomainDW <romain.ollier34@gmail.com>
2019-02-02 13:42:51 +0100

    display the "Add trick form"

--- src/Controller/CreateTrickController.php
+++ src/Controller/CreateTrickController.php
@@ -8,17 +8,27 @@
 
 namespace App\Controller;
 
+use App\Entity\Trick;
+use App\Form\AddTrickFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CreateTrickController extends AbstractController
 {
     /**
+     * @param Request $request
      * @return \Symfony\Component\HttpFoundation\Response
      * @Route("/trick/add", name="app_create_trick")
      */
-    public function index()
+    public function index(Request $request)
     {
-        return $this->render('trick/create.html.twig');
+        $trick = new Trick();
+        $form = $this->createForm(AddTrickFormType::class, $trick);
+        $form->handleRequest($request);
+
+        return $this->render('trick/create.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
