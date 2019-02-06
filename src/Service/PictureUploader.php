da9ab623cabfa46cb19fe04b45f07ace712df1d2
RomainDW <romain.ollier34@gmail.com>
2019-02-06 19:04:20 +0100

    crating a service to upload images

---
+++ src/Service/PictureUploader.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Image;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+class PictureUploader
+{
+    private $targetDirectory;
+
+    public function __construct($targetDirectory)
+    {
+        $this->targetDirectory = $targetDirectory;
+    }
+
+    public function upload(Image $file)
+    {
+        if (!$file->getFile() instanceof UploadedFile) {
+            return null;
+        }
+
+        $fileName = md5(uniqid()).'.'.$file->getFile()->guessExtension();
+
+        try {
+            $file->getFile()->move($this->getTargetDirectory(), $fileName);
+        } catch (FileException $e) {
+            // ... handle exception if something happens during file upload
+        }
+
+        return $fileName;
+    }
+
+    public function getTargetDirectory()
+    {
+        return $this->targetDirectory;
+    }
+}
