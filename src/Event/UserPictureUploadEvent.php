bcca5c542f9998d51e9d584ee992d49e1ffa0875
RomainDW <romain.ollier34@gmail.com>
2019-02-21 20:09:17 +0100

    Creation of the Picture upload event
    Signed-off-by: RomainDW <romain.ollier34@gmail.com>

---
+++ src/Event/UserPictureUploadEvent.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: romain
+ * Date: 2/19/19
+ * Time: 7:19 PM.
+ */
+
+namespace App\Event;
+
+use App\DTO\PictureDTO;
+use Symfony\Component\EventDispatcher\Event;
+
+class UserPictureUploadEvent extends Event
+{
+    public const NAME = 'user_picture.upload';
+
+    protected $picture;
+
+    public function __construct(PictureDTO $picture)
+    {
+        $this->picture = $picture;
+    }
+
+    public function getPicture()
+    {
+        return $this->picture;
+    }
+}
