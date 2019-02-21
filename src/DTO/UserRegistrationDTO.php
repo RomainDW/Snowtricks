4d5ec47171dbc88a148c6a0ec8abc661ea63bbd6
RomainDW <romain.ollier34@gmail.com>
2019-02-21 20:10:20 +0100

    Adding the pictureDTO to the constructor of the UserRegistrationDTO
    Signed-off-by: RomainDW <romain.ollier34@gmail.com>

--- src/DTO/UserRegistrationDTO.php
+++ src/DTO/UserRegistrationDTO.php
@@ -15,22 +15,25 @@ class UserRegistrationDTO
     public $password;
     public $vkey;
     public $roles;
+    public $picture;
 
     /**
      * UserRegistrationDTO constructor.
      *
-     * @param $username
-     * @param $email
-     * @param $password
-     * @param null  $vkey
-     * @param array $roles
+     * @param string     $username
+     * @param string     $email
+     * @param string     $password
+     * @param string     $vkey
+     * @param PictureDTO $picture
+     * @param array      $roles
      */
-    public function __construct($username, $email, $password, $vkey = null, $roles = ['ROLE_USER_NOT_VERIFIED'])
+    public function __construct(string $username, string $email, string $password, string $vkey = null, PictureDTO $picture = null, array $roles = ['ROLE_USER_NOT_VERIFIED'])
     {
         $this->username = $username;
         $this->password = $password;
         $this->email = $email;
         $this->roles = $roles;
         $this->vkey = $vkey;
+        $this->picture = $picture;
     }
 }
