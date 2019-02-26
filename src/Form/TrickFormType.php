c11938c3cc4a3b1adf026413058cee2495088583
RomainDW <romain.ollier34@gmail.com>
2019-02-26 22:11:48 +0100

    Remove unnecessary use of Trick Entity
    Signed-off-by: RomainDW <romain.ollier34@gmail.com>

--- src/Form/TrickFormType.php
+++ src/Form/TrickFormType.php
@@ -9,7 +9,6 @@
 namespace App\Form;
 
 use App\DTO\CreateTrickDTO;
-use App\Entity\Trick;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
