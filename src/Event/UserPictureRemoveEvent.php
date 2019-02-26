a055efa2607ad96c975d97f0accb2b6eec283529
RomainDW <romain.ollier34@gmail.com>
2019-02-26 22:00:29 +0100

    Creation of an Event that remove user picture
    Signed-off-by: RomainDW <romain.ollier34@gmail.com>

---
+++ src/Event/UserPictureRemoveEvent.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: romain
+ * Date: 2/24/19
+ * Time: 4:27 PM.
+ */
+
+namespace App\Event;
+
+use App\Entity\ImageInterface;
+use Symfony\Component\EventDispatcher\Event;
+
+class UserPictureRemoveEvent extends Event implements ImageUploadInterface
+{
+    public const NAME = 'picture.remove';
+
+    protected $image;
+
+    public function __construct(ImageInterface $image)
+    {
+        $this->image = $image;
+    }
+
+    public function getImage()
+    {
+        return $this->image;
+    }
+}
