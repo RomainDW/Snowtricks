5fd6f9523176674f49cac86267289bda35323428
RomainDW <romain.ollier34@gmail.com>
2019-03-02 13:16:08 +0100

    Rename modal items (conflicts with bootstrap classes)
    Signed-off-by: RomainDW <romain.ollier34@gmail.com>

--- assets/js/pages/show_trick.js
+++ assets/js/pages/show_trick.js
@@ -15,7 +15,7 @@ $(function () {
     });
 
     // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
+    var span = document.getElementsByClassName("close-modal")[0];
 
     // When the user clicks on <span> (x), close the modal
     span.onclick = function() {
