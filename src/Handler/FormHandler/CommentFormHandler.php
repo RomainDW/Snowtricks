83073c1d934b4866703029a1cb6d808988cded20
RomainDW <romain.ollier34@gmail.com>
2019-03-05 18:48:31 +0100

    Add Comment & Verification Form handler
    Signed-off-by: RomainDW <romain.ollier34@gmail.com>

---
+++ src/Handler/FormHandler/CommentFormHandler.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: romain
+ * Date: 3/4/19
+ * Time: 8:07 PM.
+ */
+
+namespace App\Handler\FormHandler;
+
+use App\Entity\Comment;
+use App\Entity\Trick;
+use App\Entity\User;
+use App\Repository\CommentRepository;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Security;
+
+class CommentFormHandler
+{
+    private $security;
+    private $commentRepository;
+    private $url_generator;
+    private $flashBag;
+
+    /**
+     * CommentFormhandler constructor.
+     *
+     * @param Security              $security
+     * @param CommentRepository     $commentRepository
+     * @param UrlGeneratorInterface $url_generator
+     * @param FlashBagInterface     $flashBag
+     */
+    public function __construct(Security $security, CommentRepository $commentRepository, UrlGeneratorInterface $url_generator, FlashBagInterface $flashBag)
+    {
+        $this->security = $security;
+        $this->commentRepository = $commentRepository;
+        $this->url_generator = $url_generator;
+        $this->flashBag = $flashBag;
+    }
+
+    /**
+     * @param FormInterface $form
+     * @param Comment       $comment
+     * @param User          $user
+     * @param Trick         $trick
+     * @param $slug
+     *
+     * @return bool|RedirectResponse
+     *
+     * @throws \Exception
+     */
+    public function handle(FormInterface $form, Comment $comment, User $user, Trick $trick, $slug)
+    {
+        if ($form->isSubmitted() && $form->isValid() && $this->security->isGranted('ROLE_USER')) {
+            $comment->setCreatedAt(new \DateTime());
+            $comment->setUser($user);
+            $comment->setTrick($trick);
+
+            $this->commentRepository->save($comment);
+
+            $this->flashBag->add('success', 'Le commentaire a bien été ajouté !');
+
+            return new RedirectResponse($this->url_generator->generate('app_show_trick', ['slug' => $slug, '_fragment' => 'comments']));
+        } else {
+            return false;
+        }
+    }
+}
