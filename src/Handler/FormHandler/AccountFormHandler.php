afa4f43548f23e05c290948da7c8c6a1bced9e4e
RomainDW <romain.ollier34@gmail.com>
2019-03-06 22:27:05 +0100

    Use validator to check if the data are correct
    Signed-off-by: RomainDW <romain.ollier34@gmail.com>

--- src/Handler/FormHandler/AccountFormHandler.php
+++ src/Handler/FormHandler/AccountFormHandler.php
@@ -13,10 +13,12 @@ use App\Event\UserPictureRemoveEvent;
 use App\Event\UserPictureUploadEvent;
 use App\Repository\UserRepository;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\Form\FormError;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class AccountFormHandler
 {
@@ -24,6 +26,7 @@ class AccountFormHandler
     private $userRepository;
     private $flashBag;
     private $url_generator;
+    private $validator;
 
     /**
      * AccountFormHandler constructor.
@@ -32,19 +35,33 @@ class AccountFormHandler
      * @param UserRepository           $userRepository
      * @param FlashBagInterface        $flashBag
      * @param UrlGeneratorInterface    $url_generator
+     * @param ValidatorInterface       $validator
      */
-    public function __construct(EventDispatcherInterface $dispatcher, UserRepository $userRepository, FlashBagInterface $flashBag, UrlGeneratorInterface $url_generator)
+    public function __construct(EventDispatcherInterface $dispatcher, UserRepository $userRepository, FlashBagInterface $flashBag, UrlGeneratorInterface $url_generator, ValidatorInterface $validator)
     {
         $this->dispatcher = $dispatcher;
         $this->userRepository = $userRepository;
         $this->flashBag = $flashBag;
         $this->url_generator = $url_generator;
+        $this->validator = $validator;
     }
 
     public function handle(FormInterface $form, User $user)
     {
         if ($form->isSubmitted() && $form->isValid()) {
             $updatedUserDTO = $form->getData();
+
+            $pictureViolations = $this->validator->validate($updatedUserDTO->picture, null, ['update_account']);
+
+            if (0 !== count($pictureViolations)) {
+                foreach ($pictureViolations as $violation) {
+                    $form->get('picture')->addError(new FormError($violation->getMessage()));
+                }
+
+                $updatedUserDTO->picture->setFile(null);
+
+                return false;
+            }
 
             if (null !== $updatedUserDTO->picture && null !== $updatedUserDTO->picture->getFile()) {
                 if (null !== $user->getPicture()) {
@@ -55,8 +72,18 @@ class AccountFormHandler
                 }
                 $uploadEvent = new UserPictureUploadEvent($updatedUserDTO->picture);
                 $this->dispatcher->dispatch(UserPictureUploadEvent::NAME, $uploadEvent);
-                $updatedUserDTO->picture->setFile(null);
                 $updatedUserDTO->picture->setAlt('Photo de profil de '.$user->getUsername());
+                $updatedUserDTO->picture->setFile(null);
+            }
+
+            $violations = $this->validator->validate($updatedUserDTO, null, ['update_account']);
+
+            if (0 !== count($violations)) {
+                foreach ($violations as $violation) {
+                    $this->flashBag->add('error', $violation->getMessage());
+                }
+
+                return false;
             }
 
             $user->updateFromDTO($updatedUserDTO);
@@ -64,8 +91,9 @@ class AccountFormHandler
             $this->flashBag->add('success', 'Votre compte a bien été modifié !');
 
             return new RedirectResponse($this->url_generator->generate('app_account'));
-        } else {
-            return false;
+
         }
+
+        return false;
     }
 }
